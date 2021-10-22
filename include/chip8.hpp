7a0493fdd84d46521f0d8f07c235dbd55459a3bf
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-22 19:55:45 +0200

    Random number generator changed

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -5,13 +5,25 @@
 
 const unsigned int VIDEO_HEIGHT = 32;
 const unsigned int VIDEO_WIDTH = 64;
-
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
 
 class Chip8{
 	public:
 		Chip8();
 		void LoadROM(char const* file);
 		void Cycle();
+
+		uint8_t input_keys[KEY_COUNT]{};
+		uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{};
+
+	private:
+		void Table0();
+		void Table8();
+		void TableE();
+		void TableF();
 
 		/** Components from the Chip-8 */
 		uint8_t registers[16];
@@ -22,15 +34,7 @@ class Chip8{
 		uint8_t stack_pointer;
 		uint8_t delay_timer;
 		uint8_t sound_timer;
-		uint8_t input_keys[16];
-		uint32_t video[64*32];
 		uint16_t opcode;
-
-	private:
-		void Table0();
-		void Table8();
-		void TableE();
-		void TableF();
 
 		/** http://devernay.free.fr/hacks/chip8/C8TECH10.HTM */
 		void CLS(); 	void RET(); 	void OEXE();
@@ -48,8 +52,8 @@ class Chip8{
 		
 		void XXX(); // Illegal opcode
 
-		std::default_random_engine randGen;
-		std::uniform_int_distribution<uint8_t> randByte;
+		//std::default_random_engine randGen;
+		//std::uniform_int_distribution<uint8_t> randByte;
 
 		typedef void (Chip8::*Chip8Func)();
 		Chip8Func table[0xF + 1]{&Chip8::XXX};
